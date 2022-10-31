b27000ce581d1543aebce1e92b406ce3851b9ab7
Kemal Ahmed <goatandsheep@gmail.com>
2022-10-31 14:11:14 -0400

    init commit adding code for test

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {APP_LOGGING} from 'react-native-dotenv';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working onyour app! {APP_LOGGING}.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
