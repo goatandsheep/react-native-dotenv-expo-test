3b1c457631dff754d6a00840bcad8aeed4d59f73
Kemal Ahmed <goatandsheep@gmail.com>
2023-02-21 15:32:56 -0500

    updated expo to v48

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,7 @@ module.exports = function(api) {
         'module:react-native-dotenv',
         {
           moduleName: 'react-native-dotenv',
-          verbose: true,
+          verbose: false,
         },
       ],
     ],
